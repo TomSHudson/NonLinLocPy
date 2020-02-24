f9c22255f8b0a05bba7424181e7709788ea27c15
“TomSHudson” <tomhudson92@gmail.com>
2020-02-24 15:09:44 +0000

    Initial commit.

---
+++ NonLinLocPy/__init__.py
@@ -0,0 +1,7 @@
+name = "NonLinLocPy"
+__version__ = "0.0.1"
+__description__ = "NonLinLocPy - A package for reading NonLinLoc data into python"
+__license__ = "MIT"
+__author__ = "Tom Hudson"
+__email__ = "thomas.hudson@earth.ox.ac.uk"
+import NonLinLocPy.read_nonlinloc
